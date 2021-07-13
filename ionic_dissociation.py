13a9864f29773f7361e42d801fadf4867b8629b0
ThiagoA20 <67110486+ThiagoA20@users.noreply.github.com>
2021-07-13 13:02:32 -0300

    Delete ionic_dissociation.py

--- ionic_dissociation.py
+++
@@ -1,5 +0,0 @@
-"""
-O fisico-químico Arrhenius propôs que em solução aquosa uma base e um sal sofreriam dissociação iônica, ou seja, os
-íons que já existem e que compõem um sal ou base são separados pela água, enquanto um ácido em solução aquosa sofreria
-ionização, ou seja, ele quebraria a ligação covalente
-"""
